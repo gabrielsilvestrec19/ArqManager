c72551ad15e076e1154e3d38722d5742a5518460
Gabriel Silvestre <gabrielsilvestrecoelho2@gmail.com>
2026-09-22 10:39:47 -0300

    chore: estrutura inicial do projeto e configuracao do application.yaml

---
+++ src/main/java/br/com/gabriel/arqmanager/ArqmanagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.arqmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArqmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabriel/arqmanager/ArqmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.arqmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArqmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
